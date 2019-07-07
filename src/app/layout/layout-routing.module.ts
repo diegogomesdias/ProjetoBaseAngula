11bb32c6ce618704f3fa52e69515a3d0e4e0ecad
Diego Dias <diegogomesdias.00@gmail.com>
2019-07-07 01:50:36 -0300

    Primeiro comentario

---
+++ src/app/layout/layout-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
+            { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) }
+        ]}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule {}
